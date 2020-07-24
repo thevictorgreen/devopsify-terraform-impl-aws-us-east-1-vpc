03f604640146052cbac44d73ae8bc4baf2c8043d
Victor Green <thevictorgreen@gmail.com>
2020-07-24 00:55:29 -0400

    further parameterization

--- tf_backend.tf
+++ tf_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "onekloud-AAAAA-infra"
+    bucket = "OOOOO-AAAAA-infra"
     key    = "AAAAA/AAAAA-us-east-1-vpc-terraform.tfstate"
     region = "us-east-1"
   }
